db88c2e62a71d80be430a7fc7b589a3f2d2cf0c9
paarth17032000 <paarth17032000@gmail.com>
2022-03-31 17:03:51 +0530

    pages added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Landing from './pages/Landing';
+import About from './pages/About';
+import Team from './pages/Team';
+import Sponsors from './pages/Sponsors';
 
 function App() {
   return (
-    <h1 className="font-regular text-center mt-4">
-      Zealicon 2022
-    </h1>
+    <div className='App'>
+      <Landing />
+      <About />
+      <Team />
+      <Sponsors />
+    </div>
   );
 }
 
---
+++ src/pages/Sponsors/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Sponsors() {
+  return (
+    <div className='fullscreen'>Sponsors</div>
+  )
+}
