c7fb9cbdbdaee8e66ed0b6c91f0dd7103a226e7f
paarth17032000 <paarth17032000@gmail.com>
2022-04-07 13:51:32 +0530

    zeal id paged modified

--- client/src/pages/UserPage/index.js
+++ client/src/pages/UserPage/index.js
@@ -24,11 +24,12 @@ export default function UserPage() {
     }, [])
   return (
     <div className="fullscreen login p-100 d-flex flex-column justify-content-center align-items-center text-white font-48">
+        <div className='font-bold font-48 text-nameColor'>Hey Paarth,</div>
         <div className="form-bg mx-2 text-center">
             <div className="font-bold font-30 text-white">{details.zeal_id}</div>
-            <div className="font-bold font-24 text-white mt-4">{`${details.first_name} ${details.last_name}`}</div>
+            <div className="font-bold font-24 text-white mt-4">{details.first_name}</div>
         </div>
-        <div className='d-flex flex-row-lg flex-column w-full mt-4'>
+        <div className='d-flex flex-md-row flex-column w-full mt-4'>
             <div className="form-bg mx-2 text-center mt-2">
                 <div className="font-bold font-36 text-white">WOW</div>
             </div>
--- client/src/pages/ZealIdPage/index.js
+++ client/src/pages/ZealIdPage/index.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import addIcon from '../../assets/bg/AddIcon.svg'
+import findIcon from '../../assets/bg/findIcon.svg'
 
 export default function ZealIDPage() {
   return (
-    <div className="fullscreen login p-100 d-flex flex-column flex-row-lg justify-content-center align-items-center text-white font-48">
+    <div className="fullscreen login p-100 d-flex flex-sm-row flex-column justify-content-center align-items-center text-white font-48">
         <Link to='/register' className='text-decoration-none'>
-            <div className="form-bg mx-2 text-center">
-                <div className="font-bold font-30 text-white">Make your Zeal ID</div>
+            <div className="box-bg m-5 center flex-column">
+                <img src={addIcon} alt="addIcon" className='img-fluid box-img mx-0' />
+                <div className="font-demi font-20 text-white mt-4">Make your Zeal ID</div>
             </div>
         </Link>
         <Link to='/login' className='text-decoration-none'>
-            <div className="form-bg text-center">
-                <div className="font-bold font-30 text-white">Find your Zeal ID</div>
+            <div className="box-bg center flex-column">
+                <img src={findIcon} alt="findIcon" className='img-fluid box-img mx-0' />
+                <div className="font-demi font-20 text-white mt-4">Find your Zeal ID</div>
             </div>
         </Link>
         
