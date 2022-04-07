37e09e0118dc1ec3c4ed0ac6b24ffa42a3624a26
Ayush Kumar <2580ayush2580@gmail.com>
2022-04-07 15:16:17 +0530

    navbar fixed

--- client/src/pages/ZealIdPage/index.js
+++ client/src/pages/ZealIdPage/index.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function ZealIDPage() {
   return (
     <div className="fullscreen login p-100 d-flex flex-column flex-row-lg justify-content-center align-items-center text-white font-48">
-        <Link to='/register' className='text-decoration-none'>
-            <div className="form-bg mx-2 text-center">
-                <div className="font-bold font-30 text-white">Make your Zeal ID</div>
-            </div>
-        </Link>
-        <Link to='/login' className='text-decoration-none'>
-            <div className="form-bg text-center">
-                <div className="font-bold font-30 text-white">Find your Zeal ID</div>
-            </div>
-        </Link>
-        
+      <Link to="/register" className="text-decoration-none">
+        <div className="form-bg m-4 text-center">
+          <div className="font-bold font-30 text-white">Make your Zeal ID</div>
+        </div>
+      </Link>
+      <Link to="/login" className="text-decoration-none">
+        <div className="form-bg text-center">
+          <div className="font-bold font-30 text-white">Find your Zeal ID</div>
+        </div>
+      </Link>
     </div>
-  )
+  );
 }
