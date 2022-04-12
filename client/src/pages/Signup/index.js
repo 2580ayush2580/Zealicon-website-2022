44215fb68f5e228f8ccc30f3fbaf5f459ac1d535
Ayush Kumar <2580ayush2580@gmail.com>
2022-04-12 11:51:38 +0530

    validator added

--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -5,6 +5,7 @@ import { RegisterUser } from "../../redux/actions/auth";
 import { generateOrder, login, makePayment, register } from "../../redux/api";
 import { Link } from "react-router-dom";
 import ZealiconLogo from "../../assets/image/zealicon-logo.svg";
+import validator from "validator";
 
 export default function Signup() {
   const initialData = {
@@ -23,7 +24,7 @@ export default function Signup() {
       [e.target.name]: e.target.value,
     });
   };
-  
+
   let history = useNavigate();
 
   function loadScript(src) {
@@ -78,17 +79,16 @@ export default function Signup() {
           contact_no: user.contact_no,
           college: user.college,
           admission_no: user.admission_no,
-          email: user.email
+          email: user.email,
         };
 
         const result = await makePayment(data);
-        console.log('result',result);
-        if(result.email){
-          history("/user")
-        }
-        else{
-          history("/register")
-          alert("Payment Failed")
+        console.log("result", result);
+        if (result.email) {
+          history("/user");
+        } else {
+          history("/register");
+          alert("Payment Failed");
         }
       },
       theme: {
@@ -100,13 +100,24 @@ export default function Signup() {
     const paymentObject = new window.Razorpay(options);
     paymentObject.open();
 
-    
     history("/user");
   }
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    displayRazorpay()
+    if (validator.isEmail(user.email)) {
+      if (
+        validator.isMobilePhone(user.contact_no) &&
+        user.contact_no.length === 10
+      ) {
+        displayRazorpay();
+      } else {
+        alert("Enter valid Contact!");
+      }
+    } else {
+      alert("Enter valid Email!");
+    }
+
     // const formData = new FormData();
     // formData.append("fullname", user.full_name);
     // formData.append("contact_no", user.contact_no);
@@ -120,14 +131,14 @@ export default function Signup() {
     //   // formData.append("email",user.email);
     //   // formData.append("password",user.password);
     //   // login(formData).then(() => {
-        
+
     //   // })
     // })
   };
 
   return (
     <div className="login">
-    <Navbar
+      <Navbar
         className="navbar-bg navbar-dark custom-navbar"
         bg="light"
         expand="lg"
@@ -143,91 +154,97 @@ export default function Signup() {
                 <span className="navbar-text font-demi"></span>
               </Nav.Link>
             </Nav> */}
-              <Nav>
-                <Nav.Link href="/events" className="d-flex justify-content-end w-100">
-                  <Link className="text-decoration-none" to='/login'>
-                    <span className="font-regular font-18"><span className="text-nameColor">Already Registered?</span><span className="navbar-text font-demi">&nbsp;Sign In</span></span> 
-                  </Link>
-                </Nav.Link>
-              </Nav>
+            <Nav>
+              <Nav.Link
+                href="/events"
+                className="d-flex justify-content-end w-100"
+              >
+                <Link className="text-decoration-none" to="/login">
+                  <span className="font-regular font-18">
+                    <span className="text-nameColor">Already Registered?</span>
+                    <span className="navbar-text font-demi">&nbsp;Sign In</span>
+                  </span>
+                </Link>
+              </Nav.Link>
+            </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-    <div className="nav-fullscreen  d-flex justify-content-center align-items-center text-white font-48">
-      <div className="form-bg">
-        <div className="font-regular font-16 text-white">Welcome</div>
-        <div className="font-bold font-30 text-white">Register now</div>
-        <form onSubmit={handleSubmit}>
-          <div className="d-flex flex-column">
-            <div className="font-regular font-16 text-labelColor mt-4">
-              Full name
+      <div className="nav-fullscreen  d-flex justify-content-center align-items-center text-white font-48">
+        <div className="form-bg">
+          <div className="font-regular font-16 text-white">Welcome</div>
+          <div className="font-bold font-30 text-white">Register now</div>
+          <form onSubmit={handleSubmit}>
+            <div className="d-flex flex-column">
+              <div className="font-regular font-16 text-labelColor mt-4">
+                Full name
+              </div>
+              <input
+                onChange={handleChange}
+                name="full_name"
+                className="input-container font-regular font-14 text-white mt-1"
+                type={"text"}
+                placeholder={"John"}
+              />
             </div>
-            <input
-              onChange={handleChange}
-              name="full_name"
-              className="input-container font-regular font-14 text-white mt-1"
-              type={"text"}
-              placeholder={"John"}
-            />
-          </div>
-          <div className="d-flex flex-column">
-            <div className="font-regular font-16 text-labelColor mt-3">
-              Phone
+            <div className="d-flex flex-column">
+              <div className="font-regular font-16 text-labelColor mt-3">
+                Phone
+              </div>
+              <input
+                onChange={handleChange}
+                name="contact_no"
+                className="input-container font-regular font-14 text-white mt-1"
+                type={"text"}
+                placeholder="8923xx2234"
+              />
             </div>
-            <input
-              onChange={handleChange}
-              name="contact_no"
-              className="input-container font-regular font-14 text-white mt-1"
-              type={"text"}
-              placeholder="8923xx2234"
-            />
-          </div>
-          <div className="d-flex flex-column">
-            <div className="font-regular font-16 text-labelColor mt-3">
-              Email
+            <div className="d-flex flex-column">
+              <div className="font-regular font-16 text-labelColor mt-3">
+                Email
+              </div>
+              <input
+                onChange={handleChange}
+                name="email"
+                className="input-container font-regular font-14 text-white mt-1"
+                type={"email"}
+                placeholder="John.snow@gmail.com."
+              />
             </div>
-            <input
-              onChange={handleChange}
-              name="email"
-              className="input-container font-regular font-14 text-white mt-1"
-              type={"email"}
-              placeholder="John.snow@gmail.com."
-            />
-          </div>
-          <div className="d-flex flex-column">
-            <div className="font-regular font-16 text-labelColor mt-3">
-              College
+            <div className="d-flex flex-column">
+              <div className="font-regular font-16 text-labelColor mt-3">
+                College
+              </div>
+              <input
+                onChange={handleChange}
+                name="college"
+                className="input-container font-regular font-14 text-white mt-1"
+                type={"text"}
+                placeholder="JSSATE Noida"
+              />
             </div>
-            <input
-              onChange={handleChange}
-              name="college"
-              className="input-container font-regular font-14 text-white mt-1"
-              type={"text"}
-              placeholder="JSSATE Noida"
-            />
-          </div>
-          <div className="d-flex flex-column">
-            <div className="font-regular font-16 text-labelColor mt-3">
-              Admission Number
+            <div className="d-flex flex-column">
+              <div className="font-regular font-16 text-labelColor mt-3">
+                Admission Number
+              </div>
+              <input
+                onChange={handleChange}
+                name="admission_no"
+                className="input-container font-regular font-14 text-white mt-1"
+                type={"text"}
+                placeholder="ex: 19CSE001"
+              />
             </div>
-            <input
-              onChange={handleChange}
-              name="admission_no"
-              className="input-container font-regular font-14 text-white mt-1"
-              type={"text"}
-              placeholder="ex: 19CSE001"
-            />
-          </div>
-          <div className="d-flex justify-content-center">
-            <input
-              className="input-btn mt-5 font-bold text-white font-18"
-              type={"submit"}
-              value="REGISTER NOW"
-            />
-          </div>
-        </form>
+            <div className="d-flex justify-content-center">
+              <input
+                className="input-btn mt-5 font-bold text-white font-18"
+                type={"submit"}
+                value="REGISTER NOW"
+              />
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
