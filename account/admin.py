1c0979e4556f1837eeca0d886b1dd97904c7a231
OjusWiZard <ojuswimail@gmail.com>
2022-04-15 16:49:21 +0530

    unpaid filter
    Signed-off-by: OjusWiZard <ojuswimail@gmail.com>

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,44 @@
+import os
+
 from django.contrib import admin
 
 from .models import Participant, UserDetails
+
+from payments.razorpay_utils import get_all_payments
+
+
+class UnpaidZealIDs(admin.SimpleListFilter):
+    title = "Payment not found"
+    parameter_name = "unpaid_zeal_ids"
+
+    def lookups(self, request, model_admin):
+        return (("yes", "Yes"), ("no", "No"))
+
+    def queryset(self, request, queryset):
+        if self.value() == "yes":
+            amount = os.environ.get("FEE_AMOUNT")
+            amount = int(amount) * 100
+
+            payments = get_all_payments()
+            payment_emails = [
+                payment["email"].lower()
+                for payment in payments
+                if payment["amount"] == amount
+            ]
+            payment_numbers = [
+                payment["contact"][-10:]
+                for payment in payments
+                if payment["amount"] == amount
+            ]
+
+            for participant in queryset:
+                email = participant.email
+                contact_no = participant.contact_no
+
+                if email in payment_emails and contact_no in payment_numbers:
+                    queryset = queryset.exclude(id=participant.id)
+
+        return queryset
 
 
 class ParticipantAdmin(admin.ModelAdmin):
@@ -23,6 +61,7 @@ class ParticipantAdmin(admin.ModelAdmin):
     ]
     actions = ["generate_zeal_id"]
     readonly_fields = ["zeal_id"]
+    list_filter = [UnpaidZealIDs]
 
     def generate_zeal_id(self, request, queryset):
         for participant in queryset:
